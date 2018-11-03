c45f48888121e8b56e82cbce8c73d9b7644380a3
Tiago Horta <tiago_horta95@hotmail.com>
2018-11-03 10:58:37 +0000

    05 - Add animation when opening the drawer
    react-nav-drawer/src/Drawer/components/SideBar/SideBar.js
        - Add openDrawer state in order to control whenever the drawer is opened or closed
        - Add handleOpenDrawer method to change openDrawer state
        - Add openDrawer const in render
        - Add onClick method to call the handleOpenDrawer method to open or close the drawer
        - Change burgerIcon div width to 64px in order to not change by the opening of the drawer
        - Add transition animation when opening the drawer
        - Add openedMainDiv width size

--- react-nav-drawer/src/Drawer/components/SideBar/SideBar.js
+++ react-nav-drawer/src/Drawer/components/SideBar/SideBar.js
@@ -10,7 +10,7 @@ import Icon from "./components/Icon";
 export default class SideBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { selectedIcon: "" };
+    this.state = { openDrawer: false, selectedIcon: "" };
   }
 
   createIconsArray = () => {
@@ -27,15 +27,20 @@ export default class SideBar extends Component {
       selectedIcon: iconIndex
     });
   };
+  handleOpenDrawer = () => {
+    this.setState(prevState => ({
+      openDrawer: !prevState.openDrawer
+    }));
+  };
 
   render() {
-    const { selectedIcon } = this.state;
+    const { openDrawer, selectedIcon } = this.state;
     const burgerIcon = classNames("icon-ic_burger", css(styles.burgerIcon));
     const iconsArray = this.createIconsArray();
     return (
-      <div className={css(styles.mainDiv)}>
+      <div className={css(styles.mainDiv, openDrawer && styles.opennedMainDiv)}>
         <div className={css(styles.burgerIconDiv)}>
-          <div className={burgerIcon} />
+          <div className={burgerIcon} onClick={this.handleOpenDrawer} />
         </div>
         {iconsArray.map((icon, index) => {
           return (
@@ -64,7 +69,7 @@ const styles = StyleSheet.create({
     display: "flex",
     height: 64,
     justifyContent: "center",
-    width: "100%"
+    width: 64
   },
   Icon: {
     fontSize: 24,
@@ -76,6 +81,10 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     height: "100%",
+    transition: "width 0.4s",
     width: 64
+  },
+  openedMainDiv: {
+    width: 240
   }
 });
